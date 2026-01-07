05f3478ce90a7bc49da5e0b56acf72c2452008f0
aditya-arcot <dev@aarcot.com>
2026-01-07 12:46:33 -0600

    add cleanup scripts

---
+++ scripts/cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+BASE_DIR="$SCRIPT_DIR/.."
+CONFIG_DIR="$BASE_DIR/config"
+
+source "$CONFIG_DIR/.env"
+export REGISTRY_URL="https://registry.${DOMAIN}"
+export CONTAINER="registry"
+
+./cleanup_images.sh
--- scripts/cleanup_images.sh
+++ scripts/cleanup_images.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-
-set -euo pipefail
+set -Eeuo pipefail
 
 PATH=/usr/local/bin:$PATH
 PATH=/opt/homebrew/bin:$PATH
 
-REGISTRY_URL="localhost:5001"
-KEEP_TAGS=3
+if [ -z ${REGISTRY_URL:-} ]; then
+    echo "REGISTRY_URL must be set. Use cleanup.sh or cleanup-dev.sh"
+    exit 1
+fi
+echo "REGISTRY_URL: $REGISTRY_URL"
 
-echo "disabling TLS"
-regctl registry set --tls disabled ${REGISTRY_URL}
+if [ -z ${CONTAINER:-} ]; then
+    echo "CONTAINER must be set. Use cleanup.sh or cleanup-dev.sh"
+    exit 1
+fi
+echo "CONTAINER: $CONTAINER"
+
+KEEP_TAGS=3
+echo "Keeping last ${KEEP_TAGS} tags per repository"
 echo
 
 echo "repos:"
@@ -38,4 +46,4 @@ for REPO in ${REPOS}; do
 done
 
 echo "garbage collecting"
-docker exec registry bin/registry garbage-collect /etc/docker/registry/config.yml --delete-untagged
+docker exec $CONTAINER bin/registry garbage-collect /etc/docker/registry/config.yml --delete-untagged
