ff71ade0d4c6f7319ba3da3739996877501327e0
aditya-arcot <dev@aarcot.com>
2026-01-07 16:15:22 -0600

    create config file from template

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,10 @@ set -Eeuo pipefail
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 BASE_DIR="$SCRIPT_DIR/.."
 CONFIG_DIR="$BASE_DIR/config"
+ENV_FILE="$CONFIG_DIR/.env"
+
+export $(grep -v '^#' "$ENV_FILE" | xargs)
+envsubst < "$CONFIG_DIR/config.yml.template" > "$CONFIG_DIR/config.yml"
 
 docker network create traefik-public || true
-docker compose --env-file "$CONFIG_DIR/.env" -f "$BASE_DIR/docker-compose.yml" up -d
+docker compose --env-file "$ENV_FILE" -f "$BASE_DIR/docker-compose.yml" up -d
